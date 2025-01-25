a319d463f8310c883ddfbab93b65ac90f1dee0b8
linncharm <linncharm9@gmail.com>
2025-01-26 01:37:21 +0800

    feat: 增加了自定义插件，用于获得窗口信息

---
+++ plugins/mock/README.md
@@ -0,0 +1,2 @@
+# 根据node-windows-manager插件修改而来
+## 移除了不必要的方法
---
+++ plugins/mock/test.js
@@ -0,0 +1,5 @@
+const { windowManager } = require("./dist/index");
+
+
+const window = windowManager.getActiveWindow();
+console.log(window);
